399fe57b7ede1077da512c1add351240e03a87ea
Carlos Manuel González <cmglezpdev@gmail.com>
2023-12-29 16:03:48 -0500

    feature(): add users in the vpn-server

--- vpn-server.py
+++ vpn-server.py
@@ -1,55 +1,76 @@
-from tcpprotocol import TCPProtocol
-from udpprotocol import UDPProtocol
+import json
+import os
+
+from protocols.udpprotocol import UDPProtocol
+from core import User, VPNData
+
 
 class VPNServer:
-    def __init__(self):
-        self.users = {} # Diccionario para almacenar los usuarios y sus VLANs
-        self.vlan_restrictions = {} # Diccionario para restricciones de VLAN
-        self.user_restrictions = {} # Diccionario para restricciones de usuario
+    def __init__(self, protocol: UDPProtocol):
+        self.users = []
+        self.protocol = protocol
 
-    def start(self, protocol_type):
-        if protocol_type == 'use_tcp':
-            self.protocol = TCPProtocol('localhost', 12345)
-        elif protocol_type == 'use_upd':
-            self.protocol = UDPProtocol('localhost', 12345)
-        else:
-            print('Protocol is not supported.')
-            return
-        print(f"VPN server started using {protocol_type}") 
+    def start(self):
+        print('Starting VPN server...')
 
-        while True:
-            command = input('Enter command: ').split()
-            if command[0] == 'create_user':
-                name, password, vlan_id = command[1], command[2], command[3]
-                self.create_user(name, password, vlan_id)
-            elif command[0] == 'resctrict_vlan':
-                vlan_id, ip_network = command[1], command[2]
-                self.restrict_vlan(vlan_id, ip_network)
-            elif command[0] == 'restrict_user':
-                user_id, ip_network = command[1], command[2]
-                self.restrict_user(user_id, ip_network)
-            elif command[0] == 'stop':
-                self.stop()
-                break
-            else:
-                print('Invalid command')      
+        for data in self.protocol.start():
+            try:
+                vpn_data = VPNData.to_instance(json.loads(data))
+                self.__request(vpn_data)
+            except Exception as e:
+                print(f"Error: {e}")
+                continue
 
-
-    def create_user(self, name, password, vlan_id):
-        self.users[name] = (password, vlan_id)
-        print(f"User {name} created with VLAN {vlan_id}")
-
-    def restrict_vlan(self, vlan_id, ip_network):
-        self.vlan_restrictions[vlan_id].add(ip_network)
-        print(f"VLAN ${vlan_id} was restricted by IP ${ip_network}")
-
-    def restrict_user(self, user_id, ip_network):
-        self.user_restrictions[user_id].add(ip_network)
-        print(f"User ${user_id} was restricted by IP ${ip_network}")
-        
     def stop(self):
         self.protocol.stop()
+        print('VPN server stopped')
 
+    def create_user(self, id: int, username: str, password: str, vlan_id: int):
+        if any(user.id == id for user in self.users):
+            print('User already registered')
+            return
 
-# server = VPNServer()
-# server.start(protocol_type='use_tcp')
+        self.users.append(User(id, username, password, vlan_id))
+        print(f"User {username} created with VLAN {vlan_id}")
+
+    def remove_user(self, id):
+        if not any(user.id == id for user in self.users):
+            print(f"The user with id {id} not exists")
+            return
+
+        self.users = list(filter(lambda x: x.id == id, self.users))
+
+    def __request(self, vpn_data: VPNData):
+        user = next(
+            filter(lambda x: x.username == vpn_data.username and x.password == vpn_data.password, self.users), None
+        )
+
+        if user is None:
+            print('User not found or password incorrect')
+            return
+
+        self.protocol.send(vpn_data.data, vpn_data.dip, vpn_data.dport)
+
+    def __read_users(self) -> list[User]:
+        data_path: str = 'data/users.json'
+        if not os.path.exists(data_path):
+            return []
+
+        try:
+            with open(data_path, 'r') as file:
+                return list(
+                    map(
+                        lambda x: User.to_instance(x),
+                        json.loads(file.read())
+                    )
+                )
+
+        except Exception as e:
+            print(f"Error: Corrupt Data: {e}")
+            return []
+
+    def __write_users(self):
+        data_path: str = 'data/users.json'
+
+        with open(data_path, 'w') as file:
+            json.dump(self.users, file, default=lambda x: x.__dict__)
