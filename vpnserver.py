d1dd584bf2051a643237636407bb101d80e141f0
Carlos Manuel González <cmglezpdev@gmail.com>
2023-12-29 20:14:09 -0500

    refactor(): some details

--- vpnserver.py
+++ vpnserver.py
@@ -4,15 +4,16 @@ import os
 from protocols.udpprotocol import UDPProtocol
 from core.users import User
 from core.vpndata import VPNData
+import core.constants as constants
 
 
 class VPNServer:
     def __init__(self, protocol: UDPProtocol):
-        self.users = []
+        self.users = self.__read_users()
         self.protocol = protocol
 
     def start(self):
-        print('Starting VPN server...')
+        print('VPN server started...')
 
         for data in self.protocol.start():
             try:
@@ -32,6 +33,7 @@ class VPNServer:
             return
 
         self.users.append(User(id, username, password, vlan_id))
+        self.__write_users()
         print(f"User {username} created with VLAN {vlan_id}")
 
     def remove_user(self, id):
@@ -40,6 +42,8 @@ class VPNServer:
             return
 
         self.users = list(filter(lambda x: x.id == id, self.users))
+        self.__write_users()
+        print(f"User with id {id} removed")
 
     def __request(self, vpn_data: VPNData):
         user = next(
@@ -53,12 +57,11 @@ class VPNServer:
         self.protocol.send(vpn_data.data, vpn_data.dip, vpn_data.dport)
 
     def __read_users(self) -> list[User]:
-        data_path: str = 'data/users.json'
-        if not os.path.exists(data_path):
+        if not os.path.exists(constants.USERS_DATA_PATH):
             return []
 
         try:
-            with open(data_path, 'r') as file:
+            with open(constants.USERS_DATA_PATH, 'r') as file:
                 return list(
                     map(
                         lambda x: User.to_instance(x),
@@ -71,7 +74,5 @@ class VPNServer:
             return []
 
     def __write_users(self):
-        data_path: str = 'data/users.json'
-
-        with open(data_path, 'w') as file:
+        with open(constants.USERS_DATA_PATH, 'w') as file:
             json.dump(self.users, file, default=lambda x: x.__dict__)
