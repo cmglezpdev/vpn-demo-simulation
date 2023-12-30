5e81248e65eee2d878ba6887cc698a1b40f90b7e
Carlos Manuel González <cmglezpdev@gmail.com>
2023-12-30 18:10:01 -0500

    refactor(core): remove id for the users

--- core/users.py
+++ core/users.py
@@ -5,16 +5,14 @@ class User:
     This class represents a User object.
 
     Attributes:
-        id (int): The unique identifier for the user.
         username (str): The username of the user.
         password (str): The password of the user.
         vlan_id (int): The VLAN ID associated with the user.
     """
-    def __init__(self, id: int, username: str, password: str, vlan_id: int):
+    def __init__(self, username: str, password: str, vlan_id: int):
         self.username = username
         self.password = password
         self.vlan_id = vlan_id
-        self.id = id
 
     @staticmethod
     def to_instance(user_data_dict: dict) -> 'User':
@@ -28,7 +26,6 @@ class User:
             User: A new User instance.
         """
         return User(
-            user_data_dict['id'],
             user_data_dict['username'],
             user_data_dict['password'],
             user_data_dict['vlan_id']
