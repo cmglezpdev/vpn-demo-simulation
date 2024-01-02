061afcdda738c9e31ed8f3fbffb53664aa42c19c
Carlos Manuel González <cmglezpdev@gmail.com>
2024-01-02 13:00:20 -0500

    feature()[feature/add-sqlite]: remove methods that used json database by sqlite database

--- vpnserver.py
+++ vpnserver.py
@@ -1,16 +1,13 @@
 import json
-import os
-from pathlib import Path
 from protocols.udpprotocol import UDPProtocol
-from core.users import User
+from core.users import UsersDbOperations
 from core.vpndata import VPNData
-import core.constants as constants
 
 
 class VPNServer:
     def __init__(self, protocol: UDPProtocol):
-        self.users = self.__read_users()
         self.protocol = protocol
+        self.db_users = UsersDbOperations()
 
     def start(self):
         print('VPN server started...')
@@ -25,65 +22,41 @@ class VPNServer:
 
     def stop(self):
         self.protocol.close()
+        self.db_users.close()
         print('VPN server stopped')
 
     def create_user(self, username: str, password: str, vlan_id: int):
-        if any(user.username == username for user in self.users):
-            print('User already registered')
+        user_exists = self.db_users.get_user(username)
+
+        if user_exists:
+            print('User already registered.')
             return
 
-        self.users.append(User(username, password, vlan_id))
-        self.__write_users()
+        self.db_users.create_user(username, password, vlan_id)
         print(f"User {username} created with VLAN {vlan_id}")
 
     def remove_user(self, username: str):
-        if not any(user.username == username for user in self.users):
+        user_exists = self.db_users.get_user(username)
+
+        if not user_exists:
             print(f"The user with username {username} not exists")
             return
 
-        self.users = list(filter(lambda x: x.username == username, self.users))
-        self.__write_users()
+        self.db_users.remove_user(username)
         print(f"User {username} removed")
 
     def list_users(self, vlan_id: int = None):
-        filtered_users = list(filter(
-            lambda x: vlan_id is None or x.vlan_id == vlan_id,
-            self.users
-        ))
+        users = self.db_users.list_users(vlan_id)
+
         print("Users: ")
-        for user in filtered_users:
+        for user in users:
             print(f"Username: {user.username}, VLAN: {user.vlan_id}")
 
     def __redirect(self, vpn_data: VPNData):
-        user = next(
-            filter(lambda x: x.username == vpn_data.username and x.password == vpn_data.password, self.users), None
-        )
+        logged = self.db_users.login(vpn_data.username, vpn_data.password)
 
-        if user is None:
+        if logged:
             print('User not found or password incorrect')
             return
 
         self.protocol.send(vpn_data.data, vpn_data.dip, vpn_data.dport)
-
-    def __read_users(self) -> list[User]:
-        if not os.path.exists(constants.USERS_DATA_PATH):
-            return []
-
-        try:
-            with open(constants.USERS_DATA_PATH, 'r') as file:
-                return list(
-                    map(
-                        lambda x: User.to_instance(x),
-                        json.loads(file.read())
-                    )
-                )
-
-        except Exception as e:
-            print(f"Error: Corrupt Data: {e}")
-            return []
-
-    def __write_users(self):
-        Path(constants.USERS_DATA_PATH).parent.mkdir(parents=True, exist_ok=True)
-
-        with open(constants.USERS_DATA_PATH, 'w+') as file:
-            json.dump(self.users, file, default=lambda x: x.__dict__)
