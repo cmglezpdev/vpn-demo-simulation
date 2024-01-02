e8bc0e43408a3285b8b03392bb52166fe3ecdcd2
Carlos Manuel González <cmglezpdev@gmail.com>
2024-01-02 18:25:20 -0500

    fix()[feature/add-sqlite]: remove global db instances

--- vpnserver.py
+++ vpnserver.py
@@ -8,8 +8,6 @@ from core.vpndata import VPNData
 class VPNServer:
     def __init__(self, protocol: UDPProtocol):
         self.protocol = protocol
-        self.db_users = UsersDbOperations()
-        self.db_rules = RestrictionsDbOperations()
 
     def start(self):
         print('VPN server started...')
@@ -24,51 +22,63 @@ class VPNServer:
 
     def stop(self):
         self.protocol.close()
-        self.db_users.close()
         print('VPN server stopped')
 
     def create_user(self, username: str, password: str, vlan_id: int):
-        user_exists = self.db_users.get_user(username)
+        db_users = UsersDbOperations()
+
+        user_exists = db_users.get_user(username)
 
         if user_exists:
             print('User already registered.')
             return
 
-        self.db_users.create_user(username, password, vlan_id)
+        db_users.create_user(username, password, vlan_id)
         print(f"User {username} created with VLAN {vlan_id}")
 
     def remove_user(self, username: str):
-        user_exists = self.db_users.get_user(username)
+        db_users = UsersDbOperations()
+
+        user_exists = db_users.get_user(username)
 
         if not user_exists:
             print(f"The user with username {username} not exists")
             return
 
-        self.db_users.remove_user(username)
+        db_users.remove_user(username)
         print(f"User {username} removed")
 
     def list_users(self, vlan_id: int = None):
-        users = self.db_users.list_users(vlan_id)
+        db_users = UsersDbOperations()
+
+        users = db_users.list_users(vlan_id)
 
         print("Users: ")
         for user in users:
             print(f"Username: {user.username}, VLAN: {user.vlan_id}")
 
     def restrict_user(self, username: str, blocked_ip: str, blocked_port: int):
-        user = self.db_users.get_user(username)
+        db_users = UsersDbOperations()
+        db_rules = RestrictionsDbOperations()
+
+        user = db_users.get_user(username)
         if not user:
             print(f'The user {username} not exists')
             return
 
-        self.db_rules.restrict_user(user.id, blocked_ip, blocked_port)
+        db_rules.restrict_user(user.id, blocked_ip, blocked_port)
         print(f"User {username} restricted for the address {blocked_ip}:{blocked_port}")
 
     def restrict_vlan(self, vlan: int, blocked_ip: str, blocked_port: int):
-        self.db_rules.restrict_vlan(vlan, blocked_ip, blocked_port)
+        db_rules = RestrictionsDbOperations()
+
+        db_rules.restrict_vlan(vlan, blocked_ip, blocked_port)
         print(f"VLAN {vlan}'s users are restricted for the address {blocked_ip}:{blocked_port}")
 
     def list_restrictions(self, type: str = None):
-        restrictions = self.db_rules.list_restrictions(RestrictionType(type) if type else None)
+        db_rules = RestrictionsDbOperations()
+
+        restrictions = db_rules.list_restrictions(RestrictionType(type) if type else None)
         user_restrictions = list(filter(lambda x: x.type == RestrictionType.USER, restrictions))
         vlan_restrictions = list(filter(lambda x: x.type == RestrictionType.VLAN, restrictions))
 
@@ -84,7 +94,10 @@ class VPNServer:
                 print(restriction.to_string())
 
     def __redirect(self, vpn_data: VPNData):
-        logged = self.db_users.login(vpn_data.username, vpn_data.password)
+        db_users = UsersDbOperations()
+        db_rules = RestrictionsDbOperations()
+
+        logged = db_users.login(vpn_data.username, vpn_data.password)
 
         if logged:
             print('User not found or password incorrect')
@@ -92,7 +105,7 @@ class VPNServer:
 
         pass_restrictions = any(map(
             lambda x: x.check_pass(vpn_data),
-            self.db_rules.list_restrictions()
+            db_rules.list_restrictions()
         ))
 
         if pass_restrictions:
